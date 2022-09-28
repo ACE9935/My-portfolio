983155e927e50ca2beead728290534283254ff19
ACE9935 <anaselmouden99@gmail.com>
2022-09-28 17:29:29 +0200

    commit

--- components/FullAppBar.tsx
+++ components/FullAppBar.tsx
@@ -26,7 +26,7 @@ function FullAppBar({setOpen,open}:{setOpen: React.Dispatch<SetStateAction<boole
         <AnimatePresence>
         {open &&
         <motion.div
-        className='z-50 !w-screen h-screen !flex !flex-col fixed bg-[rgba(0,0,0,0.8)] backdrop-blur-md'
+        className='!z-50 !w-screen h-screen !flex !flex-col fixed bg-[rgba(0,0,0,0.8)] backdrop-blur-md'
         exit={{opacity:0,transition:{duration:0.6}}}
         initial={{opacity:0}}
         animate={{opacity:1,transition:{duration:0.6}}}
@@ -37,7 +37,7 @@ function FullAppBar({setOpen,open}:{setOpen: React.Dispatch<SetStateAction<boole
          <Box ref={ref} className="!flex !flex-col !items-center !space-y-3 !justify-center">
          <AppLogo/>
          <List className="!w-full !max-w-[50rem]">
-          {['Bio','Skills','Contact'].map(i=>(<><ListItem href={'#'+i} onClick={()=>setOpen(false)} component='a' className="cursor-pointer hover:text-[orange] transition-color duration-300 text-white !text-xl !flex !justify-center p-4">
+          {['Bio','Skills','Contact'].map(i=>(<><ListItem href={'#'+i} onClick={()=>setOpen(false)} component='a' className="cursor-pointer hover:text-[orange] transition-color duration-300 text-white !text-xl !flex !justify-center !p-4">
             {i}
           </ListItem>
           <Divider light/></>
