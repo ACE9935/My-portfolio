bcbd40749602817732ddda8e90d066a043ed8301
Anas El mouden <anas.el-mouden@etu.u-bordeaux.fr>
2024-02-29 02:05:32 +0100

    big update

--- projects-data.ts
+++ projects-data.ts
@@ -1,30 +1,37 @@
  export const data=[
+   {
+      url:'p66.jpg',
+      title:'Games app',
+      tecs:['React','Tailwind.css','Redux'],
+      subt:'Published: 1/2/2024',
+      surl:'https://app-games-coral.vercel.app/'
+   },
  {
-    url:'p1.png',
-    title:'Moroccan recipes app',
-    tecs:['React','Next.js','Bootstrap'],
-    subt:'Published: 12/04/2022',
-    surl:'https://recipes-app-three-pearl.vercel.app/'
- },
+   url:'p55.jpg',
+   title:'Blog website',
+   tecs:['React','Next.js','Tailwind.css','MUI react','Redux','GSAP','Mongodb','Node.js','React-query'],
+   subt:'Published: 12/02/2024',
+   surl:'https://inkinspirehub.vercel.app/'
+},
  {
     url:'p33.png',
     title:'Music app',
     tecs:['React','Next.js','Tailwind.css','MUI react','Redux','GSAP','Mongodb','Node.js','React-query'],
-    subt:'Published: 12/07/2022',
-    surl:'https://music-app-wb3t.vercel.app/'
+    subt:'Published: 12/07/2023',
+    surl:'https://music-app-nine-tau.vercel.app/'
  },
  {
-    url:'p2.png',
-    title:'Rock Paper Scissor game',
-    tecs:['React','Tailwind.css','Redux'],
-    subt:'Published: 12/09/2022',
-    surl:'https://rock-paper-scissor-omega.vercel.app/'
- },
+   url:'p1.png',
+   title:'Moroccan recipes app',
+   tecs:['React','Next.js','Bootstrap'],
+   subt:'Published: 12/04/2022',
+   surl:'https://recipes-app-three-pearl.vercel.app/'
+},
  {
    url:'p4.png',
    title:'Landing page',
    tecs:['React','Tailwind.css','MUI react'],
-   subt:'Published: 3/12/2022',
+   subt:'Published: 3/12/2023',
    surl:'https://app-test-tau-ruddy.vercel.app/'
 },
  ] 
