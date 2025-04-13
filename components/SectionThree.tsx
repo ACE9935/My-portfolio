14995124f7d59c166e162056b492059f141509fb
Anas El mouden <anas.el-mouden@etu.u-bordeaux.fr>
2025-04-13 16:21:09 +0200

    project cards animated

--- components/SectionThree.tsx
+++ components/SectionThree.tsx
@@ -10,20 +10,6 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 function SectionThree() {     
-  
-  gsap.registerPlugin(ScrollTrigger);
-  useEffect(()=>{
-    document.querySelectorAll('.sx-2>*').forEach(i => {
-
-      gsap.fromTo(i,{y:'30rem',opacity:0},{y:'-3.4rem',opacity:1,duration:1,
-        scrollTrigger:{
-          start:'-=900',
-          trigger: i,
-          
-        }})
-      
-    });
-  },[])
 
     return ( 
         <section id='Skills' className="px-6 flex flex-col items-center sm:px-14 pt-14 pb-36">
@@ -32,7 +18,7 @@ function SectionThree() {
         </Box>
 
        <div
-        className="max-w-[100rem] gris-cols-1 md:grid-cols-2 xl:grid-cols-3 grid gap-3"
+        className="sc max-w-[100rem] gris-cols-1 md:grid-cols-2 xl:grid-cols-3 grid gap-3"
       >
         {data.map((o,i)=><ProjectCard key={i} {...o}/>)}
       </div>
